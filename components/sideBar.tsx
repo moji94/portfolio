c0679f2144bb7eadb734bed00e5c0b2b51cfeb9e
mr-ezak <ezaksaeed@gmail.com>
2022-11-20 13:24:03 +0330

    Update

--- components/sideBar.tsx
+++ components/sideBar.tsx
@@ -4,7 +4,14 @@ import styled from 'styled-components'
 export const SideBar = (): JSX.Element => {
   return (
     <Container>
-      <p>sidebar</p>
+      <Items>
+        <SingleItem color="#FFF" size="20">
+          Hello
+        </SingleItem>
+        <SingleItem color="#FFF">Hello</SingleItem>
+        <SingleItem color="#FFF">Hello</SingleItem>
+        <SingleItem>Hello</SingleItem>
+      </Items>
     </Container>
   )
 }
@@ -17,5 +24,31 @@ const Container = styled.div`
   float: left;
   :hover {
     width: 220px;
+  }
+`
+
+const Items = styled.ul`
+  width: 100%;
+  height: 100vh;
+  background-color: red;
+  padding-left: 20px;
+`
+const SingleItem = styled.li<any>`
+  width: 100%;
+  height: 40px;
+  color: ${(c) => (c.color ? c.color : '#00F')};
+  font-size: ${(s) => (s.size ? s.size : 15)}px;
+  position: relative;
+  :hover {
+    background-color: black;
+  }
+  ::after {
+    content: 'H';
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: black;
+    position: absolute;
+    right: 0;
   }
 `
